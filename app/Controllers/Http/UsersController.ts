f581c29415ceb1b5a5a3946f38229ecd3a87af12
Mohammed Benmansour <mohammed.benmansour2002@gmail.com>
2023-12-30 12:59:49 +0100

    Removed an unused import

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,4 +1,3 @@
-import { Exception } from '@adonisjs/core/build/standalone'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { schema, rules } from '@ioc:Adonis/Core/Validator'
 import User from 'App/Models/User'
