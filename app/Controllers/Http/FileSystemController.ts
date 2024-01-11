a68d179ab0959af8b319078eb5fc31b406c2a01b
Mohammed Benmansour <benmansour.mohammed2002@gmail.com>
2024-01-11 20:35:01 +0100

    Added video streaming

--- app/Controllers/Http/FileSystemController.ts
+++ app/Controllers/Http/FileSystemController.ts
@@ -24,4 +24,26 @@ export default class FileSystemController {
     response.download('./resources/posters/' + file)
     return {}
   }
+
+  public async video({ bouncer, request, response, params, auth }: HttpContextContract) {
+    if (request.input('from') !== 'localhost') {
+      if (request.hostname() !== process.env.IMAGE_HOSTNAME) {
+        response.abort('Resource Not Found', 401)
+      }
+      await auth.use('web').check()
+      await bouncer.authorize('viewContent')
+    }
+
+    const file: string = (params['*'] + '').replaceAll(',', '/')
+
+    if (!fs.existsSync('./resources/videos/' + file)) {
+      response.abort('Resource Not Found', 404)
+      return
+    }
+
+    console.log(request.headers())
+    response.append('Accept-Ranges', 'bytes')
+    response.attachment('./resources/videos/' + file, file + 'jpg', 'inline')
+    return {}
+  }
 }
